0865e63f0d6e59285084609a08b2b6d1726c0e8b
Mikhail Shadrin <shadrin.michael@yandex.ru>
2020-11-16 04:01:11 +0300

    Separate properties by profiles, fixing CI

--- application/src/main/java/com/tja/bh/config/SecurityConfig.java
+++ application/src/main/java/com/tja/bh/config/SecurityConfig.java
@@ -15,7 +15,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import javax.sql.DataSource;
 
 
-@EnableWebSecurity(debug = true)
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     final DataSource postgresqlDataSource;
