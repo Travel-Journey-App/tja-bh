c3cb5509acdd0671f589a0ef8ab94938e4ff06a6
Mikhail Shadrin <shadrin.michael@yandex.ru>
2020-12-15 03:42:48 +0300

    Alter data scripts to enlarge description length from 255 to 2000 chars

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,3 +2,6 @@ insert into roles (id, name)
 values (1, 'ROLE_USER'),
        (0, 'ROLE_ADMIN')
 on conflict do nothing;
+
+alter table if exists event_activity
+    alter column description type varchar(2000);
